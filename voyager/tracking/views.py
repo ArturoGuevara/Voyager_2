af957b22831234646bb600c5ba586e53f1536463
EugenioSa <A01701996@itesm.mx>
2019-10-04 13:56:45 -0500

    API DHL obtener info en json e imprimir en terminal

--- voyager/tracking/views.py
+++ voyager/tracking/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 
 def index(request):
+    
+    
+    url = "https://api-eu.dhl.com/track/shipments"
+
+    headers = {
+        'Accept': 'application/json',
+        'DHL-API-Key': 'dGmqZ7RmVGHGkLWYR8y28C7qMsDtiMmn'
+        }
+    payload = {
+        'trackingNumber': '5551260643',
+        'service': 'express'
+    }
+
+    # This url is for testing 
+    url = 'https://api-eu.dhl.com/track/shipments'
+    resp = requests.get(url, params=payload, headers=headers)
+    json_data = resp.json()
+    
+
+    print(resp.content)
+    
+    
+    
     context = {
         
     }
-    return render(request, 'tracking/index.html', context)
+    return render(request, 'tracking/index.html', json_data)
 
