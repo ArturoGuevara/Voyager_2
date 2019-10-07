f1a9823aaf4acd31f84c3b653bb1f056da2d9859
barronsguillermo <A01207945@itesm.mx>
2019-10-07 11:09:54 -0500

    Añadir frontend de ingreso de muestra

--- voyager/procesamiento_reportes/urls.py
+++ voyager/procesamiento_reportes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.indexView, name='index'),
+    path('ingresar_muestras',views.ingresar_muestra,name='Ingresar muestra')
 ]
--- voyager/procesamiento_reportes/views.py
+++ voyager/procesamiento_reportes/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def indexView(request):
     return render(request, 'procesamiento_reportes/index.html')
+
+def ingresar_muestra(request):
+    return  render(request, 'procesamiento_reportes/ingresar_muestra.html')
