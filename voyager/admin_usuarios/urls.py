62d0e6ef71098fea2c79bb57219fa24a46c2a955
Alex1497 <a01207765@itesm.mx>
2019-10-07 13:19:18 -0500

    07/10/19 login

--- voyager/admin_usuarios/urls.py
+++ voyager/admin_usuarios/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login',views.loginView,name='login'),
+    path('login/',views.loginView,name='login'),
+    path('verify_login/', views.verifyLogin, name='backend_login'),
+    path('home/', views.homeView, name='home'),
 ]
--- voyager/admin_usuarios/views.py
+++ voyager/admin_usuarios/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
+#Vista de Login
 def loginView(request):
     return render(request,'admin_usuarios/login.html')
+
+#Controlador login
+def verifyLogin(request):
+    mail = request.POST['mail']
+    password = request.POST['password']
+    print(mail)
+    print(password)
+    try:
+        u = User.objects.get(email=mail)
+        print(u)
+        user = authenticate(request,username=u.username,password=password)
+        if user is not None:
+            login(request, user)
+            print('Login exitoso \n\n')
+            return redirect('home/')
+        else:
+            #Redireccionar error
+            print('Mal contraseña \n\n')
+            return render(request,'admin_usuarios/login.html', {
+                'error': 1
+            })
+    except:
+        #Redireccionar error
+        print('No existe el correo \n\n')
+        return render(request,'admin_usuarios/login.html', {
+            'error': 1
+        })
+    return 0;
+
+def homeView(request):
+    return render(request,'admin_usuarios/home.html')
