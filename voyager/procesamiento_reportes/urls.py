03bb493295cd4276257ed9b9cbd3f8ab4610b640
unknown <A01207538@itesm.mx>
2019-10-08 12:32:27 -0500

    oi formas

--- voyager/procesamiento_reportes/urls.py
+++ voyager/procesamiento_reportes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.conf.urls import urls
 from . import views
 
 urlpatterns = [
     path('', views.indexView, name='index'),
     path('ordenes_internas', views.ordenes_internas, name='ordenes_internas'),
+   	path(r'^(?P<pk>\d+)/update/$', views.oi_actualizar, name='book_update'),
+
+
 ]
--- voyager/procesamiento_reportes/views.py
+++ voyager/procesamiento_reportes/views.py
@@ -1,5 +1,10 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from procesamiento_reportes.models import OrdenInterna
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+from .forms import *
+
 
 # Create your views here.
 def indexView(request):
@@ -11,3 +16,43 @@ def ordenes_internas(request):
         'ordenes': ordenes,
     }
     return render(request, 'procesamiento_reportes/ordenes_internas.html', context)
+
+
+
+def oi_guardar(request, form, template_name):
+    data = dict()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['form_is_valid'] = True
+            ordenes_internas = OrdenInterna.objects.all()
+            data['html_book_list'] = render_to_string('books/includes/partial_book_list.html', {
+                'books': ordenes_internas
+            })
+        else:
+            data['form_is_valid'] = False
+    context = {'form': form}
+    return JsonResponse(data)
+
+
+
+def oi_info_actualizar(request, pk):
+    oi = get_object_or_404(OrdenInterna, pk=pk)
+    if request.method == 'POST':
+        form = infoForma(request.POST, instance=oi)
+    else:
+        form = infoForma(instance=oi)
+    return oi_guardar(request, form, 'books/includes/partial_book_update.html')
+
+
+
+
+
+def oi_observaciones_actualizar(request, pk):
+    oi = get_object_or_404(OrdenInterna, pk=pk)
+    if request.method == 'POST':
+        form = observacionesForma(request.POST, instance=oi)
+    else:
+        form = observacionesForma(instance=oi)
+    return oi_guardar(request, form, 'books/includes/partial_book_update.html')
+
