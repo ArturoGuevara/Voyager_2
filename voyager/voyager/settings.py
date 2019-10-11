6eaafd883012cb92da7d20fd7f425f915bfe555d
Oswaldo1002009 <A01206725@itesm.mx>
2019-10-11 16:34:56 -0500

    Añadir campos en orden.js para hacerlos visibles en las formas

--- voyager/voyager/settings.py
+++ voyager/voyager/settings.py
@@ -79,6 +79,7 @@ LOGIN_URL = '/cuentas/login'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -87,6 +88,13 @@ DATABASES = {
         'PASSWORD': 'lalocura',
         'HOST': 'localhost',
         'PORT': '5432',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
