a2f3ad0fb9eac3362d34151505faf30d7a38fca1
Juan Arturo Cruz Cardona <A01701804@itesm.mx>
2019-10-17 11:43:00 -0500

    Terminar de agregar el rbac a las views del app de ventas

--- voyager/ventas/views.py
+++ voyager/ventas/views.py
@@ -4,6 +4,7 @@ from cuentas.models import IFCUsuario
 from django.http import JsonResponse
 from django.core import serializers
 from django.http import HttpResponse
+from django.http import Http404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.decorators import login_required
@@ -11,90 +12,105 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def ver_catalogo(request):
-    analisis = Analisis.objects.all()
-    context = {
-        'analisis': analisis,
-    }
-    return render(request, 'ventas/catalogo.html', context)
-
+    user_logged = IFCUsuario.objects.get(user = request.user) # Obtener el tipo de usuario logeado
+    if user_logged.rol.nombre == "Ventas" or user_logged.rol.nombre == "SuperUser":
+        analisis = Analisis.objects.all()
+        context = {
+            'analisis': analisis,
+        }
+        return render(request, 'ventas/catalogo.html', context)
+    else: # Si el rol del usuario no es ventas no puede entrar a la página
+        raise Http404
 
 @login_required
 def cargar_analisis(request, id):
-    if request.method == 'POST':
-        analisis = Analisis.objects.get(id_analisis = id)
-        if analisis:
-            data = serializers.serialize("json", [analisis], ensure_ascii = False)
-            data = data[1:-1]
-            return JsonResponse({"data": data})
-        else:
-            response = JsonResponse({"error": "No existe ese análisis"})
-            response.status_code = 500
-            # Regresamos la respuesta de error interno del servidor
-            return response
-    else:
-        response = JsonResponse({"error": "No se mandó por el método correcto"})
-        response.status_code = 500
-        # Regresamos la respuesta de error interno del servidor
-        return response
-    
-@login_required
-def editar_analisis(request, id):
-    # Checamos que el método sea POST
-    if request.method == 'POST':
-        # Obtenemos el objeto de análisis
-        analisis = Analisis.objects.get(id_analisis = id)
-        if analisis:
-            #Que ningún campo esté vacío
-            if is_not_empty(request.POST['nombre']) and is_not_empty(request.POST['codigo']) and is_not_empty(request.POST['descripcion']) and is_not_empty(request.POST['precio']) and is_not_empty(request.POST['tiempo']):
-                # Actualizamos campos
-                analisis.nombre = request.POST['nombre']
-                analisis.codigo = request.POST['codigo']
-                analisis.descripcion = request.POST['descripcion']
-                analisis.precio = request.POST['precio']
-                analisis.tiempo = request.POST['tiempo']
-                # Guardamos cambios
-                analisis.save()
-                # Obtenemos los nuevos valores
-                analisis_actualizado = Analisis.objects.get(id_analisis = id)
-                data = serializers.serialize("json", [analisis_actualizado], ensure_ascii = False)
+    user_logged = IFCUsuario.objects.get(user = request.user) # Obtener el tipo de usuario logeado
+    if user_logged.rol.nombre == "Ventas" or user_logged.rol.nombre == "SuperUser":
+        if request.method == 'POST':
+            analisis = Analisis.objects.get(id_analisis = id)
+            if analisis:
+                data = serializers.serialize("json", [analisis], ensure_ascii = False)
                 data = data[1:-1]
-                # Regresamos información actualizada
                 return JsonResponse({"data": data})
             else:
-                response = JsonResponse({"error": "Campos vacíos"})
+                response = JsonResponse({"error": "No existe ese análisis"})
                 response.status_code = 500
                 # Regresamos la respuesta de error interno del servidor
                 return response
         else:
-            response = JsonResponse({"error": "No existe ese análisis"})
+            response = JsonResponse({"error": "No se mandó por el método correcto"})
             response.status_code = 500
             # Regresamos la respuesta de error interno del servidor
             return response
-    else:
-        response = JsonResponse({"error": "No se mandó por el método correcto"})
-        response.status_code = 500
-        # Regresamos la respuesta de error interno del servidor
-        return response
-
+    else: # Si el rol del usuario no es ventas no puede entrar a la página
+        raise Http404
+    
+@login_required
+def editar_analisis(request, id):
+    user_logged = IFCUsuario.objects.get(user = request.user) # Obtener el tipo de usuario logeado
+    if user_logged.rol.nombre == "Ventas" or user_logged.rol.nombre == "SuperUser":
+        # Checamos que el método sea POST
+        if request.method == 'POST':
+            # Obtenemos el objeto de análisis
+            analisis = Analisis.objects.get(id_analisis = id)
+            if analisis:
+                #Que ningún campo esté vacío
+                if is_not_empty(request.POST['nombre']) and is_not_empty(request.POST['codigo']) and is_not_empty(request.POST['descripcion']) and is_not_empty(request.POST['precio']) and is_not_empty(request.POST['tiempo']):
+                    # Actualizamos campos
+                    analisis.nombre = request.POST['nombre']
+                    analisis.codigo = request.POST['codigo']
+                    analisis.descripcion = request.POST['descripcion']
+                    analisis.precio = request.POST['precio']
+                    analisis.tiempo = request.POST['tiempo']
+                    # Guardamos cambios
+                    analisis.save()
+                    # Obtenemos los nuevos valores
+                    analisis_actualizado = Analisis.objects.get(id_analisis = id)
+                    data = serializers.serialize("json", [analisis_actualizado], ensure_ascii = False)
+                    data = data[1:-1]
+                    # Regresamos información actualizada
+                    return JsonResponse({"data": data})
+                else:
+                    response = JsonResponse({"error": "Campos vacíos"})
+                    response.status_code = 500
+                    # Regresamos la respuesta de error interno del servidor
+                    return response
+            else:
+                response = JsonResponse({"error": "No existe ese análisis"})
+                response.status_code = 500
+                # Regresamos la respuesta de error interno del servidor
+                return response
+        else:
+            response = JsonResponse({"error": "No se mandó por el método correcto"})
+            response.status_code = 500
+            # Regresamos la respuesta de error interno del servidor
+            return response
+    else: # Si el rol del usuario no es ventas no puede entrar a la página
+        raise Http404
+        
 @login_required
 def borrar_analisis(request, id):
-    # Checamos que el método sea POST
-    if request.method == 'POST':
-        # Obtenemos el objeto de análisis
-        analisis = Analisis.objects.get(id_analisis = id)
-        if analisis:
-            analisis.delete()
-            return HttpResponse('OK')
+    user_logged = IFCUsuario.objects.get(user = request.user) # Obtener el tipo de usuario logeado
+    if user_logged.rol.nombre == "Ventas" or user_logged.rol.nombre == "SuperUser":
+        # Checamos que el método sea POST
+        if request.method == 'POST':
+            # Obtenemos el objeto de análisis
+            analisis = Analisis.objects.get(id_analisis = id)
+            if analisis:
+                analisis.delete()
+                return HttpResponse('OK')
+            else:
+                response = JsonResponse({"error": "No existe ese análisis"})
+                response.status_code = 500
+                # Regresamos la respuesta de error interno del servidor
+                return response
         else:
-            response = JsonResponse({"error": "No existe ese análisis"})
+            response = JsonResponse({"error": "No se mandó por el método correcto"})
             response.status_code = 500
             # Regresamos la respuesta de error interno del servidor
             return response
-    else:
-        response = JsonResponse({"error": "No se mandó por el método correcto"})
-        response.status_code = 500
-        # Regresamos la respuesta de error interno del servidor
-        return response
+    else: # Si el rol del usuario no es ventas no puede entrar a la página
+        raise Http404
 
 # Funciones para validar campos
 def is_not_empty(data):
