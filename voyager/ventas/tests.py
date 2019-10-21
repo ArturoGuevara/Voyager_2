9df627ab8855ea066200ae82648aebecc8d71925
DanteMaxF <A01207543@itesm.mx>
2019-10-21 12:37:56 -0500

    Agregar nuevos tests USV06-06

--- voyager/ventas/tests.py
+++ voyager/ventas/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from ventas.forms import AnalisisForma
 from django.urls import reverse, resolve
 from .views import agregar_analisis
-from reportes.models import Analisis,Cotizacion
+from reportes.models import Analisis,Cotizacion, Pais, Nota
 from cuentas.models import Rol,IFCUsuario,Empresa
 from django.contrib.auth.models import User
 from datetime import datetime, date
@@ -10,13 +10,17 @@ from datetime import datetime, date
 # Create your tests here.
 class TestAnalisis(TestCase):
     def setUp(self):
-        analisis1 = Analisis(codigo="1", nombre="Analisis 1", descripcion="Descripcion 1", precio="1500", tiempo="3")
+
+        pais = Pais(id_pais=1, nombre="Mexico")
+        pais.save()
+
+        analisis1 = Analisis(codigo="1", descripcion="Descripcion 1", precio="1500", tiempo="3", unidad_min="500 gr", pais=Pais.objects.get(id_pais=1), acreditacion="0")
         analisis1.save()
 
-        analisis2 = Analisis(codigo="2", nombre="Analisis 2", descripcion="Descripcion 2", precio="2500", tiempo="5")
+        analisis2 = Analisis(codigo="2", descripcion="Descripcion 2", precio="2500", tiempo="5", unidad_min="500 gr", pais=Pais.objects.get(id_pais=1), acreditacion="0")
         analisis2.save()
 
-        analisis3 = Analisis(codigo="3", nombre="Analisis 3", descripcion="Descripcion 3", precio="3500", tiempo="7")
+        analisis3 = Analisis(codigo="3", descripcion="Descripcion 3", precio="3500", tiempo="7", unidad_min="500 gr", pais=Pais.objects.get(id_pais=1), acreditacion="0")
         analisis3.save()
 
 #Unit tests de US06-06 añadir análisis.
@@ -36,13 +40,16 @@ class TestAnalisis(TestCase):
     def test_analisis_forma(self):
         form_data = {
         'codigo': 'H-091233',
-        'nombre': 'Pesticida',
         'descripcion': 'Análisis para detección de pesticida en cultivos',
         'precio': 2000.00,
-        'duracion': '5-8 días'
+        'unidad_min': '500 gr',
+        'duracion': '5-8 días',
+        'pais': '1',
+        'acreditacion' : '0'
         }
         form = AnalisisForma(data = form_data)
         self.assertTrue(form.is_valid())
+
 
     #def test_anadir_analisis(self):
         #analisis = Analisis.objects.first()
