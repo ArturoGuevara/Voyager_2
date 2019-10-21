9c76af85bff4c6c06aa4bac4617330db8d93af3c
unknown <A01187388@itesm.mx>
2019-10-21 10:47:06 -0500

    Arreglar login_required

--- voyager/root/views.py
+++ voyager/root/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 
-@login required
+@login_required
 def indexView(request):
     return render(request, 'root/base.html')
