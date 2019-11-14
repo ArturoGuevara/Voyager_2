cf7715810bb059b9d6e31a514a29fa1db8de6caf
Juan Arturo Cruz Cardona <A01701804@itesm.mx>
2019-11-14 10:27:35 -0600

    Terminar Front End para ingreso de información de cliente (incluye validaciones)

--- voyager/reportes/static/reportes/js/muestras.js
+++ voyager/reportes/static/reportes/js/muestras.js
@@ -1,33 +1,55 @@
-function estado(){
-  var pais = document.getElementById("pais").value;
-  var label = document.getElementById("label-estado");
-  label.hidden = false;
-  var estado;
-  if (pais == 'México') {
-    estado = document.getElementById("estado1");
-    estado.hidden = false;
-    estado.required = true;
-    estado = document.getElementById("estado2");
-    estado.hidden = true;
-    estado.required = false;
-    estado.value = "";
-  }
-  else {
-    estado = document.getElementById("estado2");
-    estado.hidden = false;
-    estado.required = true;
-    estado = document.getElementById("estado1");
-    estado.hidden = true;
-    estado.required = false;
-    estado.value = "";
-  }
+function estado() {
+    var pais = document.getElementById("pais").value;
+    var label = document.getElementById("label-estado");
+    label.hidden = false;
+    var estado;
+    if (pais == 'México') {
+        estado = document.getElementById("estado1");
+        estado.hidden = false;
+        estado.required = true;
+        estado = document.getElementById("estado2");
+        estado.hidden = true;
+        estado.required = false;
+        estado.value = "";
+    } else {
+        estado = document.getElementById("estado2");
+        estado.hidden = false;
+        estado.required = true;
+        estado = document.getElementById("estado1");
+        estado.hidden = true;
+        estado.required = false;
+        estado.value = "";
+    }
 }
 
-function guardar_muestra(){
+function guardar_muestra() {
     var enviar = document.getElementById("enviar").value;
-    document.getElementById("enviar").value=0;
+    document.getElementById("enviar").value = 0;
 }
-function enviar_muestra(){
+
+function enviar_muestra() {
     var enviar = document.getElementById("enviar").value;
-    document.getElementById("enviar").value=1;
+    document.getElementById("enviar").value = 1;
+}
+
+function validar_info_solicitante() {
+    var dict = {
+        1: check_is_not_empty($('#nombre').val(), '#nombre'),
+        2: check_is_not_empty($('#direccion').val(), '#direccion'),
+        3: check_is_not_empty($('#pais').val(), '#pais'),
+        4: check_is_not_empty($('#idioma').val(), '#idioma'),
+    }
+
+    for (var key in dict) {
+        var value = dict[key];
+        var flag = true;
+        if (value == false) {
+            flag = false
+            break;
+        }
+    }
+
+    if (flag == true) {
+        $('#ingreso-cliente-form').submit();
+    }
 }
