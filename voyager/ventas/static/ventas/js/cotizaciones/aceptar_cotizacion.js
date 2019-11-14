4d5a8cd86f68d7b78833178dd05ddf951ada6a83
unknown <A01187388@itesm.mx>
2019-11-13 19:30:46 -0600

    Terminar controlador

--- voyager/ventas/static/ventas/js/cotizaciones/aceptar_cotizacion.js
+++ voyager/ventas/static/ventas/js/cotizaciones/aceptar_cotizacion.js
@@ -1,24 +1,24 @@
-//######### USV04-04 ########
+//######### USV16-50 ########
 
 /* Funciones que se ejecutan al cargar la página */
 $(document).ready(function() {
     // Cuando se cierra el modal para confirmar el borrado de la cotizacion, reajusta la variable global a 0
     $('#aceptar_cot').on('hidden.bs.modal', function () {
-       id_cotizacion = 0;
+       id_aceptar = 0;
     });
 });
 
 
 function aceptar_cotizacion(id){
     if (id > 0){
-        id_cotizacion = id;     // Carga el id de la cotización que se quiere aceptar en la variable global
+        id_aceptar = id;     // Carga el id de la cotización que se quiere aceptar en la variable global
     }
 }
 
 function confirmar_aceptar_cotizacion(){
-    if (id_cotizacion > 0){
+    if (id_aceptar > 0){
         // Guardar variables globales en locales
-        var id =  id_cotizacion;
+        var id =  id_aceptar;
         var token = csrftoken;
         $.ajax({
             url: "aceptar_cotizacion/"+id,
@@ -30,9 +30,9 @@ function confirmar_aceptar_cotizacion(){
             type: "POST",
             success: function(){
                 aceptar_cotizacion_tabla('.cot-row',id);
-                id_cotizacion = 0;
+                id_aceptar = 0;
                 $('#aceptar_cot').modal('toggle');                                        // Cerrar el modal de aceptar cotizacion
-                showNotification('top','right','Se ha borrado la cotización exitosamente.');
+                showNotification('top','right','Se ha validado la cotización exitosamente.');
             },
         });
 
@@ -43,7 +43,8 @@ function confirmar_aceptar_cotizacion(){
 function aceptar_cotizacion_tabla(clase,id){
     $(clase).each(function(i,e){
        if( $(e).data('id') == id ){
-           $(e).remove();
+         $(e).find('#cotizacion_child_estado').html('Aceptado');
+         $(e).find('#cotizacion_child_aceptar').prop('hidden', true);
        }
     });
 }
