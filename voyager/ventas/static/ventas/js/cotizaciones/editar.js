9a4c54952c7046a71618b903c757aa040f9ba664
unknown <A01187388@itesm.mx>
2019-11-13 21:49:18 -0600

    Corregir cotizaciones

--- voyager/ventas/static/ventas/js/cotizaciones/editar.js
+++ voyager/ventas/static/ventas/js/cotizaciones/editar.js
@@ -6,6 +6,7 @@ function restaurar_modal_ver_cot(){
     $('#btn-canc-edit-cot').removeClass('d-inline').addClass('d-none');
     $('#btn-guar-editar-cot').removeClass('d-inline').addClass('d-none');
     $('#btn-editar-cot').removeClass('d-none').addClass('d-inline');
+    $('#btn-space-edit').removeClass('d-none');
 
     // Alternar contenedores
     $('#ver-resumen-cot').removeClass('d-none').addClass('d-block');
@@ -27,9 +28,13 @@ function restaurar_modal_ver_cot(){
 }
 $('#btn-editar-cot').click(function(){
     // Alternar botones
+    $("input[name='editar-cot-an[]']:checked").each(function () {
+        che.push(parseInt($(this).val()));
+    });
     $(this).removeClass('d-inline').addClass('d-none');
     $('#btn-canc-edit-cot').removeClass('d-none').addClass('d-inline');
     $('#btn-guar-editar-cot').removeClass('d-none').addClass('d-inline');
+    $('#btn-space-edit').addClass('d-none');
 
     //Alternar contenedores
     $('#ver-resumen-cot').removeClass('d-block').addClass('d-none');
@@ -57,6 +62,11 @@ function editar_cot_eliminar_an(id){
     $('input[name="editar-cot-an[]"]').each(function (){
         if(id == $(this).data('id')){
             $(this).prop('checked', false);
+            for(var i = 0; i < che.length; i++){
+              if ( che[i] === id) {
+                che.splice(i, 1);
+              }
+            }
         }
     });
     calc_total();
@@ -76,6 +86,7 @@ $("input[name='editar-cot-an[]']").click(function (){
 
     // Si el análisis no está en la tabla agregarlo y si sí está quitarlo
     if(flag == 0){
+        che.push(parseInt($(this).val()));
         // Obtenemos el token de django para el ajax
         var token = csrftoken;
         $.ajax({
@@ -104,6 +115,11 @@ $("input[name='editar-cot-an[]']").click(function (){
                 $(this).remove();
             }
         });
+        for(var i = 0; i < che.length; i++){
+          if ( che[i] === parseInt(id)) {
+            che.splice(i, 1);
+          }
+        }
     }
 });
 
@@ -117,9 +133,10 @@ function guardar_cambios_cot(){
         var ivas = [];
         var totales = [];
         // Obtenemos las id de los análisis seleccionados
-        $("input[name='editar-cot-an[]']:checked").each(function () {
-            checked.push(parseInt($(this).val()));
-        });
+        checked = che;
+        // $("input[name='editar-cot-an[]']:checked").each(function () {
+        //     checked.push(parseInt($(this).val()));
+        // });
         // Obtenemos las cantidades de los análisis seleccionados
         $("input[name='editar-cot-cantidades[]']").each(function () {
             cantidades.push(parseInt($(this).val()));
