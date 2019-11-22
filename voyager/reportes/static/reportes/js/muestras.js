672ca56a70d9b5f27bcdb0a0d5e5221bdb95b188
Juan Arturo Cruz Cardona <A01701804@itesm.mx>
2019-11-22 12:00:07 -0600

    Agregar formato de producto microbiologia

--- voyager/reportes/static/reportes/js/muestras.js
+++ voyager/reportes/static/reportes/js/muestras.js
@@ -1,6 +1,14 @@
 var cnt_ingreso_agricola = 1;
 var cnt_ingreso_procesado = 1;
 var cnt_ingreso_microbiologia = 1;
+
+var opcionesAnalisis = '';
+for(var i = 0; i < analisis.length ;i++){
+    opcionesAnalisis+='<option value="'+analisis[i][0]+'">'+analisis[i][1]+'</option>';
+}
+var retro = '</select><div class="invalid-feedback">Seleccione un análisis</div></td>';
+var retro2 = '</select></td>';
+
 function validar_info_solicitante() {
     var dict = {
         1: check_is_not_empty($('#nombre').val(), '#nombre'),
@@ -76,45 +84,90 @@ $('.btn-plantilla').click(function(){
         $('#containerProductoProcesado').collapse('hide');
     }
 });
-function validar_producto_procesado(){
-    // Validamos los arreglos de inputs
-    $("input[name='tipoMuestra[]']").each(function(){
-        var id = $(this).data('id');
-        check_is_not_empty($(this).val(),'#tipo-muestra-'+id);
-    });
-    $("input[name='descripcionMuestra[]']").each(function(){
-        var id = $(this).data('id');
-        check_is_not_empty($(this).val(),'#descripcion-muestra-'+id);
-    });
-    $("input[name='fechaMuestreo[]']").each(function(){
-        var id = $(this).data('id');
-        check_is_not_empty($(this).val(),'#fecha-muestreo-'+id);
-        check_is_date($(this).val(),'#fecha-muestreo-'+id);
-    });
-    $("select[name='analisis1[]']").each(function(){
-        var id = $(this).data('id');
-        check_is_not_empty($(this).val(),'#primer-analisis-'+id);
-    });
-}
+
+/* FUNCIONES PARA AGREGAR Y QUITAR FILAS A CADA FORMATO */
+
 function agregar_fila_procesado(){
-    var opcionesAnalisis = '';
-    for(var i = 0; i < analisis.length ;i++){
-        opcionesAnalisis+='<option value="'+analisis[i][0]+'">'+analisis[i][1]+'</option>';
-    }
-    var retro = '</select><div class="invalid-feedback">Seleccione un análisis</div></td>';
-    var retro2 = '</select></td>';
-    
-    
-    $('#tabla-procesado-body').append('<tr class="fila-tabla-procesado" data-id="'+cnt_ingreso_procesado+'"><td><input type="text" class="form-control" data-id="'+cnt_ingreso_procesado+'" id="tipo-muestra-'+cnt_ingreso_procesado+'" name="tipoMuestra[]"><div class="invalid-feedback">Este campo no puede estar vacío</div></td><td><input type="text" class="form-control" data-id="'+cnt_ingreso_procesado+'" id="descripcion-muestra-'+cnt_ingreso_procesado+'" name="descripcionMuestra[]"><div class="invalid-feedback">Este campo no puede estar vacía</div></td><td><input type="text" class="form-control datepicker" data-id="'+cnt_ingreso_procesado+'" id="fecha-muestreo-'+cnt_ingreso_procesado+'" name="fechaMuestreo[]"><div class="invalid-feedback">Ingrese fecha con el formato DD/MM/YYYY</div></td><td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="primer-analisis-'+cnt_ingreso_procesado+'" name="analisis1[]">'+opcionesAnalisis+retro+'<td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="segundo-analisis-'+cnt_ingreso_procesado+'" name="analisis2[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+retro2+'<td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="tercer-analisis-'+cnt_ingreso_procesado+'" name="analisis3[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+retro2+'<td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="cuarto-analisis-'+cnt_ingreso_procesado+'" name="analisis4[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+retro2+'<td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="quinto-analisis-'+cnt_ingreso_procesado+'" name="analisis5[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+retro2+'<td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="sexto-analisis-'+cnt_ingreso_procesado+'" name="analisis6[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+retro2+'<td><button type="button" class="btn btn-danger" onclick="quitar_filar_procesado('+cnt_ingreso_procesado+')"><i class="fa fa-trash"></i></button></td></tr>');
+    $('#tabla-procesado-body').append('<tr class="fila-tabla-procesado" data-id="'+cnt_ingreso_procesado+'"><td><input type="text" class="form-control" data-id="'+cnt_ingreso_procesado+'" id="tipo-muestraPR-'+cnt_ingreso_procesado+'" name="tipoMuestraPR[]"><div class="invalid-feedback">Este campo no puede estar vacío</div></td><td><input type="text" class="form-control" data-id="'+cnt_ingreso_procesado+'" id="descripcion-muestraPR-'+cnt_ingreso_procesado+'" name="descripcionMuestraPR[]"><div class="invalid-feedback">Este campo no puede estar vacío</div></td><td><input type="text" class="form-control datepicker" data-id="'+cnt_ingreso_procesado+'" id="fecha-muestreoPR-'+cnt_ingreso_procesado+'" name="fechaMuestreoPR[]"><div class="invalid-feedback">Ingrese fecha con el formato DD/MM/YYYY</div></td><td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="primer-analisisPR-'+cnt_ingreso_procesado+'" name="analisis1PR[]">'+opcionesAnalisis+'</select>'+retro+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="segundo-analisisPR-'+cnt_ingreso_procesado+'" name="analisis2PR[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="tercer-analisisPR-'+cnt_ingreso_procesado+'" name="analisis3PR[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="cuarto-analisisPR-'+cnt_ingreso_procesado+'" name="analisis4PR[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="quinto-analisisPR-'+cnt_ingreso_procesado+'" name="analisis5PR[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_procesado+'" id="sexto-analisisPR-'+cnt_ingreso_procesado+'" name="analisis6PR[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><button type="button" class="btn btn-danger" onclick="quitar_fila_procesado('+cnt_ingreso_procesado+')"><i class="fa fa-trash"></i></button></td></tr>');
     
     cnt_ingreso_procesado+=1;
     // El datepicker que se agrega activarlo
     $(".datepicker" ).datepicker();
 }
-function quitar_filar_procesado(id){
+function quitar_fila_procesado(id){
     $('.fila-tabla-procesado').each(function(){
         if(id == $(this).data('id')){
             $(this).remove();
         }
+    });
+}
+
+function agregar_fila_micro(){    
+    $('#tabla-microbiologia-body').append('<tr class="fila-tabla-micro" data-id="'+cnt_ingreso_microbiologia+'"><td><input type="text" class="form-control" data-id="'+cnt_ingreso_microbiologia+'" id="tipo-muestraMB-'+cnt_ingreso_microbiologia+'" name="tipoMuestraMB[]"><div class="invalid-feedback">Este campo no puede estar vacío</div></td><td><input type="text" class="form-control" data-id="'+cnt_ingreso_microbiologia+'" id="lote-codigoMB-'+cnt_ingreso_microbiologia+'" name="loteCodigoMB[]"><div class="invalid-feedback">Este campo no puede estar vacío</div></td><td><input type="text" class="form-control" data-id="'+cnt_ingreso_microbiologia+'" id="muestreadorMB-'+cnt_ingreso_microbiologia+'" name="muestreadorMB[]"><div class="invalid-feedback">Este campo no puede estar vacío</div></td><td><input type="text" class="form-control datepicker" data-id="'+cnt_ingreso_microbiologia+'" id="fecha-muestreoMB-'+cnt_ingreso_microbiologia+'" name="fechaMuestreoMB[]"><div class="invalid-feedback">Ingrese fecha con el formato DD/MM/YYYY</div></td><td><select class="custom-select" data-id="'+cnt_ingreso_microbiologia+'" id="primer-analisisMB-'+cnt_ingreso_microbiologia+'" name="analisis1MB[]">'+opcionesAnalisis+'</select>'+retro+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_microbiologia+'" id="segundo-analisisMB-'+cnt_ingreso_microbiologia+'" name="analisis2MB[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_microbiologia+'" id="tercer-analisisMB-'+cnt_ingreso_microbiologia+'" name="analisis3MB[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_microbiologia+'" id="cuarto-analisisMB-'+cnt_ingreso_microbiologia+'" name="analisis4MB[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_microbiologia+'" id="quinto-analisisMB-'+cnt_ingreso_microbiologia+'" name="analisis5MB[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><select class="custom-select" data-id="'+cnt_ingreso_microbiologia+'" id="sexto-analisisMB-'+cnt_ingreso_microbiologia+'" name="analisis6MB[]"><option value="-1" selected>Ninguno</option>'+opcionesAnalisis+'</select>'+retro2+'</td><td><input type="text" class="form-control" data-id="'+cnt_ingreso_microbiologia+'" id="metodo-referenciaMB-'+cnt_ingreso_microbiologia+'" name="metodoReferenciaMB[]"><div class="invalid-feedback">Este campo no puede estar vacío</div></td><td><button type="button" class="btn btn-danger" onclick="quitar_fila_micro('+cnt_ingreso_microbiologia+')"><i class="fa fa-trash"></i></button></td></tr>');
+    
+    cnt_ingreso_microbiologia+=1;
+    // El datepicker que se agrega activarlo
+    $(".datepicker" ).datepicker();
+}
+function quitar_fila_micro(id){
+    $('.fila-tabla-micro').each(function(){
+        if(id == $(this).data('id')){
+            $(this).remove();
+        }
+    });
+}
+
+/* FUNCIONES PARA VALIDAR LOS CAMPOS DE CADA FORMATO */
+function validar_ingreso_muestra(){
+    validar_producto_procesado();
+    validar_producto_microbiologia();
+    // validar_producto_agricola();
+}
+function validar_producto_procesado(){
+    // Validamos los arreglos de inputs
+    $("input[name='tipoMuestraPR[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#tipo-muestraPR-'+id);
+    });
+    $("input[name='descripcionMuestraPR[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#descripcion-muestraPR-'+id);
+    });
+    $("input[name='fechaMuestreoPR[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#fecha-muestreoPR-'+id);
+        //check_is_date($(this).val(),'#fecha-muestreoPR-'+id);
+    });
+    $("select[name='analisis1PR[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#primer-analisisPR-'+id);
+    });
+}
+function validar_producto_microbiologia(){
+    // Validamos los arreglos de inputs
+    $("input[name='tipoMuestraMB[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#tipo-muestraMB-'+id);
+    });
+    $("input[name='loteCodigoMB[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#lote-codigoMB-'+id);
+    });
+    $("input[name='fechaMuestreoMB[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#fecha-muestreoMB-'+id);
+        //check_is_date($(this).val(),'#fecha-muestreoMB-'+id);
+    });
+    $("input[name='muestreadorMB[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#muestreadorMB-'+id);
+    });
+    $("select[name='analisis1MB[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#primer-analisisMB-'+id);
+    });
+    $("input[name='metodoReferenciaMB[]']").each(function(){
+        var id = $(this).data('id');
+        check_is_not_empty($(this).val(),'#metodo-referenciaMB-'+id);
     });
 }
