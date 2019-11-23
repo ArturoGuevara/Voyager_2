1a6d10a8a30987bea66242399c6c43d6132b7ff9
barronsguillermo <A01207945@itesm.mx>
2019-11-22 23:24:03 -0600

    Terminar controlador recuperar contraseña

--- voyager/voyager/settings.py
+++ voyager/voyager/settings.py
@@ -148,7 +148,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
@@ -166,3 +166,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+SENDGRID_API_KEY = "API_KEY_recuperar_contra.txt"
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'A01207945@itesm.mx'
